2dc36d40423f8164b79b3a8ed2c774d29089d1c0
Misha Frunze <misha.frunze@eezeepeezee.ru>
2020-05-14 12:58:25 +0300

    Add .nice-label styling

---
+++ src/components/form-fields/form-fields.js
@@ -0,0 +1,26 @@
+/* Find all the text inputs on the page */
+let textInputs = document.querySelectorAll('.nice-label input[type=email], .nice-label input[type=file], .nice-label input[type=image], .nice-label input[type=password], .nice-label input[type=range], .nice-label input[type=search], .nice-label input[type=tel], .nice-label input[type=text], .nice-label input[type=url], .nice-label textarea');
+textInputs = [].slice.call(textInputs);
+
+/* Find all the labels and place them into array too */
+const labels = [];
+
+textInputs.forEach((eachInput) => {
+  labels.push(eachInput.parentNode.previousSibling);
+});
+
+/* Listen for focus and blur events to toggle class */
+
+textInputs.forEach((eachInput, index) => {
+  eachInput.addEventListener('focus', () => {
+    if (!labels[index].classList.contains('moved')) {
+      labels[index].classList.add('moved');
+    }
+  });
+
+  eachInput.addEventListener('blur', (e) => {
+    if (e.target.value === '' || e.target.value === undefined) {
+      labels[index].classList.remove('moved');
+    }
+  });
+});
